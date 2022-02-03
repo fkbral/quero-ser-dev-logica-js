ea862695f188b4cb2864690ca583e4946934d3a3
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-02-03 15:49:45 -0300

    feat: aula 02 - operações matemáticas e breve discussão sobre var

---
+++ aula-02/operacoes-matematicas/demais-operacoes/index.js
@@ -0,0 +1,15 @@
+// subtração
+const subtracao = 100 - 20
+console.log('subtraindo 100 de 20 temos: ' + subtracao)
+
+// multiplicação
+const multiplicacao = 3 * 4
+console.log('multiplicando 3 com 4 temos: ' + multiplicacao)
+
+// divisão
+const divisao = 10 / 3
+console.log('dividindo 10 por 3 temos: ' + divisao)
+
+// resto da divisão (sempre retorna um número inteiro)
+const resto = 10 % 3
+console.log('o resto da divisão inteira de 10 por 3 é: ' + resto)
---
+++ aula-02/operacoes-matematicas/soma/index.js
@@ -0,0 +1,57 @@
+// const numeroInteiro = 40
+// const numeroReal = 10.5
+
+// console.log(numeroInteiro)
+// console.log(numeroReal)
+
+// console.log("Olá " + "usuário")
+// const soma = numeroInteiro + numeroReal
+
+// console.log(soma)
+
+// se somo 2 + 3 como strings
+// poderíamos pensar que o resultado esperado seria 5
+// mas como a soma é feita como uma concatenação de strings
+// resulta em 23
+
+// Devemos tratar essa conversão de string para número
+// Como?
+// 3 formas:
+// - função parseInt()
+// - função parseFloat()
+// - função Number()
+// cuidado: não podemos somar números com textos, pois o js converterá tudo
+// para textos
+
+// não podemos somar algo que não conseguiu ser convertido para número
+// pelo parseInt(), parseFloat(), Number() com um número
+// - isto vai nos retornar um valor que não é um número
+// ou mais especificamente o NaN do JavaScript (Not a Number)
+
+// Resolução 1
+// const primeiroNumero = prompt('Digite o primeiro número:')
+// console.log(primeiroNumero)
+
+// const segundoNumero = prompt('Digite o segundo número:')
+// console.log(segundoNumero)
+
+// const primeiroNumeroTratado = parseInt(primeiroNumero)
+// const segundoNumeroTratado = parseInt(segundoNumero)
+
+// console.log(primeiroNumeroTratado)
+// console.log(segundoNumeroTratado)
+
+// const soma = primeiroNumeroTratado + segundoNumeroTratado
+// console.log(soma)
+
+// Resolução 2
+const primeiroNumero = parseFloat(prompt('Digite o primeiro número:'))
+console.log(primeiroNumero)
+
+const segundoNumero = parseFloat(prompt('Digite o segundo número:'))
+console.log(segundoNumero)
+
+const soma = primeiroNumero + segundoNumero
+console.log(soma)
+
+const subratracao = 
