7e5a80817d80e662e0e397c24baefa4794034aa6
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-02-07 18:22:46 -0300

    feat: aula 03 - resolução exercício 7

--- exercicios/07/index.js
+++ exercicios/07/index.js
@@ -1,5 +1,21 @@
-function dobraNumero(numero) {
-
+// alternativa a
+function dobraNumero(numeroParaDobrar) {
+  return numeroParaDobrar * 2
 }
 
-dobraNumero(10)
+// alternativa b
+const dobraNumeroArrow = (numeroParaDobrar) => {
+  return numeroParaDobrar * 2
+}
+
+// alternativa c
+const dobraNumeroSemParametros = () => {
+  const numeroParaDobrar = Number(
+    prompt('Entre com um número para dobrar: ')
+  )
+  return numeroParaDobrar * 2
+}
+
+console.log(dobraNumero(10))
+console.log(dobraNumeroArrow(10))
+console.log(dobraNumeroSemParametros())
